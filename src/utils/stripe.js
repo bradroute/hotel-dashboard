e7e2c1b67da78bbe8818f09350e11ea0c70e8787
Brad Route <bradroute@Brads-MacBook-Pro.local>
2025-09-22 23:24:57 -0500

    Fix

--- src/utils/stripe.js
+++ src/utils/stripe.js
@@ -1,8 +1,6 @@
 // src/utils/stripe.js
 import { loadStripe } from '@stripe/stripe-js';
 
-console.log('💳 STRIPE KEY:', process.env.REACT_APP_STRIPE_PK);
-
 export const stripePromise = loadStripe(
   process.env.REACT_APP_STRIPE_PK
 );
